ee13ba2da96e42e43e7e7cdb4e493ad9ad02381b
Polovoy Vlad <vlad.polovoy@gmail.com>
2024-03-27 13:27:11 +0200

    edit, create  notes; style

--- SharedNotes/Model/Note.cs
+++ SharedNotes/Model/Note.cs
@@ -1,10 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace SharedNotes.Model;
 
 public class Note
 {
+    [Required]
     public int Id { get; set; }
-    public string Title { get; set; }
-    public string Body { get; set; }
+    
+    [Required]
+    [StringLength(200, ErrorMessage = "Title too long.")]
+    public string Title { get; set; } = String.Empty;
+    
+    [StringLength(65530, ErrorMessage = "Title too long.")]
+    public string Body { get; set; } = String.Empty;
+    
+    [Required]
     public DateTime Created { get; set; }
+    
     public DateTime? LastEdit { get; set; }
 }
